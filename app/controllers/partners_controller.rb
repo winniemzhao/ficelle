41a4bd8fc41e08e26b729a438eb6106949da5086
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-23 14:22:55 -0400

    new, create methods for partner and a simple form, need to events index view page(dashboard)

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,8 +1,16 @@
 class PartnersController < ApplicationController
   def new
+    @partner = Partner.new
   end
 
   def create
+    @partner = Partner.new(params_partner)
+    @partner.user = current_user
+    if @partner.save
+      redirect to dashboard_path
+    else
+      render :new, status: unprocessable_entity
+    end
   end
 
   def edit
@@ -12,5 +20,11 @@ class PartnersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def params_partner
+    params.require(:partner).permit(:name, :birthday, :phone_number, :email, :location)
   end
 end
