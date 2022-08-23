e1783635ee20636d5fb29f3d12833fd8d1a7bcae
Michael Wales <wales@aya.yale.edu>
2022-08-23 09:22:26 -0700

    progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "destroying all this crap"
+User.destroy_all
+
+puts "making two users"
+user1 = User.create!(email: "a@gmail.com", password: "123456", name: "John", phone_number: "1234567890", location: "Montreal")
+
+user2 = User.create!(email: "b@gmail.com", password: "123456", name: "Michael", phone_number: "(514) 458-9946", location: "Toronto")
+
+puts "making 2 partners for user1"
+partner1 = Partner.new(name: "Gillian", birthday: Date.new(1980,01,01), phone_number: "1234567", email: "c@gmail.com", location: "Montreal")
+partner1.user = user1
+partner1.save!
+
+partner2 = Partner.new(name: "April", birthday: Date.new(1990,01,01), phone_number: "#######", email: "d@gmail.com", location: "Montréal")
+partner2.user = user2
+partner2.save!
+
+puts "making 2 ingenious inspos"
+inspo1 = Inspo.create!(name: "Elephant riding in Nepal", genre: "date")
