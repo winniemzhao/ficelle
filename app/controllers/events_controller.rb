a1231f69fe8e483102bf1ab44a16aae2fa4c1f50
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-24 11:21:54 -0400

    removed env, event controller work in progress - have to change the table first.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,9 +21,13 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to dashboard_path
   end
 
   def destroy
