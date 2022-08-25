d6b2a0c33dab14cfbdfda954c67a3246de3f4329
Michael Wales <wales@aya.yale.edu>
2022-08-25 14:04:53 -0700

    partial commit

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,6 +1,6 @@
 class KeywordsController < ApplicationController
   def index
-    @partner = current_user.partner
+    @partner = current_user.partner unless current_user.partner.nil?
     @keywords = Keyword.all
   end
 end
--- app/javascript/controllers/keywords_controller.js
+++ app/javascript/controllers/keywords_controller.js
@@ -5,4 +5,12 @@ export default class extends Controller {
   connect() {
     console.log("Hello from our first Stimulus controller")
   }
+
+  toggle() {
+    if (this.element.classList.contains("keyword-outline")) {
+      this.element.classList.remove("keyword-outline");
+    } else {
+      this.element.classList.add("keyword-outline");
+    }
+  }
 }
