a0442ce5da9e33b814bc9427643dc1395b29388a
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-25 17:45:24 -0400

    wip javascrip navbar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import NavbarActiveController from "./navbar_active_controller"
+application.register("navbar-active", NavbarActiveController)
