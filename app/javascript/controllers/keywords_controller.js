675108135de5441bb62f65decad53f26381edae8
Michael Wales <wales@aya.yale.edu>
2022-08-25 14:50:55 -0700

    another partial

--- app/javascript/controllers/keywords_controller.js
+++ app/javascript/controllers/keywords_controller.js
@@ -2,15 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="keywords"
 export default class extends Controller {
+  static targets = ["cards"]
   connect() {
-    console.log("Hello from our first Stimulus controller")
   }
 
-  toggle() {
+  toggle(event) {
+    event.preventDefault()
     if (this.element.classList.contains("keyword-outline")) {
       this.element.classList.remove("keyword-outline");
     } else {
       this.element.classList.add("keyword-outline");
+      fetch(this.cardsTarget.action, {
+        method: "GET" path_to
+      })
     }
+  }
+
+  send(event) {
+    event.preventDefault()
+
+    console.log("TODO: send request in AJAX")
   }
 }
