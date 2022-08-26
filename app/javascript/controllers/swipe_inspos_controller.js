47bac23c53c727df9959fb196400e0821ac5980a
sarah fulham <sarahlfulham@gmail.com>
2022-08-25 20:20:59 -0400

    swiping logic

--- app/javascript/controllers/swipe_inspos_controller.js
+++ app/javascript/controllers/swipe_inspos_controller.js
@@ -3,15 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="swipe-inspos"
 export default class extends Controller {
   static targets = ["inspo"]
+
+  currentIndex = 0
+
   connect() {
     this.inspoTarget.classList.remove("d-none")
   }
 
   revealNextCard() {
-    this.inspoTarget.classList.add("d-none")
-   const index = parseInt(this.inspoTarget.dataset.index, 10)
-   const nextCard = this.inspoTargets[index + 1]
-   nextCard.classList.remove("d-none")
-   console.log(currentTarget.parentNode)
+    console.log(this.inspoTargets.length)
+    console.log(`index ${this.currentIndex}`)
+
+    if (this.currentIndex == this.inspoTargets.length - 1) {
+      // dont want to trigger this anyway
+    } else {
+      const currentCard = this.inspoTargets[this.currentIndex]
+      currentCard.classList.add("d-none")
+      const nextCard = this.inspoTargets[this.currentIndex += 1]
+      nextCard.classList.remove("d-none")
+    }
   }
 }
