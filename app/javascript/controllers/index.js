476212d27fc47324bd68f10bafa3e658805994ea
Michael Wales <wales@aya.yale.edu>
2022-08-25 19:21:35 -0700

    commented out HelloController

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,8 +5,8 @@
 import { application } from "./application"
 
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
 
 import SwipeInsposController from "./swipe_inspos_controller"
 application.register("swipe-inspos", SwipeInsposController)
