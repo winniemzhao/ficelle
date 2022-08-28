33b16e5489597eaee7c69a260acdc2a7fcd69ad7
Michael Wales <wales@aya.yale.edu>
2022-08-28 14:07:22 -0700

    I think this is what he was trying to achieve

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -16,8 +16,5 @@ application.register("keywords", KeywordsController)
 import LoadingController from "./loading_controller"
 application.register("loading", LoadingController)
 
-import NavbarActiveController from "./navbar_active_controller"
-application.register("navbar-active", NavbarActiveController)
-
 import SwipeInsposController from "./swipe_inspos_controller"
 application.register("swipe-inspos", SwipeInsposController)
