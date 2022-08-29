16be74f8b04efc145cb6be542ed0338f41f26c97
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-29 12:58:43 -0400

    settings wip

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get '/events/:id', to: 'events#update_success'
   get '/completed_events', to: 'events#completed_events', as: :history
   get '/us/', to: 'users#us', as: :us
+  get '/settings/', to: 'users#setting', as: :setting
   resources :inspos, only: %i[index edit update] do
     member do
       post 'toggle_favorite', to: "inspos#toggle_favorite"
