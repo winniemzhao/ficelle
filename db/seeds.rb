f4deb77e320a96becc79356feecf80daa5c052b4
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-30 10:20:47 -0400

    commented out some inspos and iks

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,70 +95,70 @@ file12 = URI.open("https://images.unsplash.com/photo-1593061231114-1798846fd643?
 inspo12.photo.attach(io: file12, filename: "inspo12.jpg", content_type: "image/jpg")
 inspo12.save!
 
-inspo13 = Inspo.new(name: "DIY Sushi Night", genre: "date", content: "The best ingredients can be found at Miyamoto", cost: 100.00)
-file13 = URI.open("https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80")
-inspo13.photo.attach(io: file13, filename: "inspo13.jpg", content_type: "image/jpg")
-inspo13.save!
+# inspo13 = Inspo.new(name: "DIY Sushi Night", genre: "date", content: "The best ingredients can be found at Miyamoto", cost: 100.00)
+# file13 = URI.open("https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80")
+# inspo13.photo.attach(io: file13, filename: "inspo13.jpg", content_type: "image/jpg")
+# inspo13.save!
 
-inspo14 = Inspo.new(name: "Meditation Retreat", genre: "gift", content: "Escape together", cost: 500.00)
-file14 = URI.open("https://images.unsplash.com/photo-1520363147109-9ab9d1c8f8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1256&q=80")
-inspo14.photo.attach(io: file14, filename: "inspo14.jpg", content_type: "image/jpg")
-inspo14.save!
+# inspo14 = Inspo.new(name: "Meditation Retreat", genre: "gift", content: "Escape together", cost: 500.00)
+# file14 = URI.open("https://images.unsplash.com/photo-1520363147109-9ab9d1c8f8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1256&q=80")
+# inspo14.photo.attach(io: file14, filename: "inspo14.jpg", content_type: "image/jpg")
+# inspo14.save!
 
-inspo15 = Inspo.new(name: "Send a Love Poem", genre: "text", content: "Every time\nI see your face\nit reminds\nme of you.")
-file15 = URI.open("https://images.unsplash.com/photo-1517867065801-e20f409696b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80")
-inspo15.photo.attach(io: file15, filename: "inspo15.jpg", content_type: "image/jpg")
-inspo15.save!
+# inspo15 = Inspo.new(name: "Send a Love Poem", genre: "text", content: "Every time\nI see your face\nit reminds\nme of you.")
+# file15 = URI.open("https://images.unsplash.com/photo-1517867065801-e20f409696b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80")
+# inspo15.photo.attach(io: file15, filename: "inspo15.jpg", content_type: "image/jpg")
+# inspo15.save!
 
-inspo16 = Inspo.new(name: "Netflix and Chill", genre: "date", content: "🎬🍿😏", cost: 20.00)
-file16 = URI.open("https://sm.askmen.com/t/askmen_in/article/n/netflix-an/netflix-and-chill_xjgm.1200.jpg")
-inspo16.photo.attach(io: file16, filename: "inspo16.jpg", content_type: "image/jpg")
-inspo16.save!
+# inspo16 = Inspo.new(name: "Netflix and Chill", genre: "date", content: "🎬🍿😏", cost: 20.00)
+# file16 = URI.open("https://sm.askmen.com/t/askmen_in/article/n/netflix-an/netflix-and-chill_xjgm.1200.jpg")
+# inspo16.photo.attach(io: file16, filename: "inspo16.jpg", content_type: "image/jpg")
+# inspo16.save!
 
-inspo17 = Inspo.new(name: "Explore the Botanical Gardens", genre: "date", content: "An oasis in the middle of the city", cost: 45.00, location: "4101 Sherbrooke Street East, Montréal, Quebec")
-file17 = URI.open("https://d1q0twczwkl2ie.cloudfront.net/wp-content/uploads/2015/03/new.jpg")
-inspo17.photo.attach(io: file17, filename: "inspo17.jpg", content_type: "image/jpg")
-inspo17.save!
+# inspo17 = Inspo.new(name: "Explore the Botanical Gardens", genre: "date", content: "An oasis in the middle of the city", cost: 45.00, location: "4101 Sherbrooke Street East, Montréal, Quebec")
+# file17 = URI.open("https://d1q0twczwkl2ie.cloudfront.net/wp-content/uploads/2015/03/new.jpg")
+# inspo17.photo.attach(io: file17, filename: "inspo17.jpg", content_type: "image/jpg")
+# inspo17.save!
 
-inspo18 = Inspo.new(name: "Stroll along the Lachine Canal", genre: "date", content: "For a 2-hour walk, make Atwater Market your goal from McGill and de la Commune")
-file18 = URI.open("https://media.timeout.com/images/105897435/1372/1029/image.jpg")
-inspo18.photo.attach(io: file18, filename: "inspo18.jpg", content_type: "image/jpg")
-inspo18.save!
+# inspo18 = Inspo.new(name: "Stroll along the Lachine Canal", genre: "date", content: "For a 2-hour walk, make Atwater Market your goal from McGill and de la Commune")
+# file18 = URI.open("https://media.timeout.com/images/105897435/1372/1029/image.jpg")
+# inspo18.photo.attach(io: file18, filename: "inspo18.jpg", content_type: "image/jpg")
+# inspo18.save!
 
-inspo19 = Inspo.new(name: "Mixology Kit", genre: "gift", content: "Why pay $15 for a drink at the bar when your partner can make you one for free?", cost: 127.00)
-file19 = URI.open("https://www.pamperedchef.ca/iceberg/com/product/100013-lg.jpg")
-inspo19.photo.attach(io: file19, filename: "inspo19.jpg", content_type: "image/jpg")
-inspo19.save!
+# inspo19 = Inspo.new(name: "Mixology Kit", genre: "gift", content: "Why pay $15 for a drink at the bar when your partner can make you one for free?", cost: 127.00)
+# file19 = URI.open("https://www.pamperedchef.ca/iceberg/com/product/100013-lg.jpg")
+# inspo19.photo.attach(io: file19, filename: "inspo19.jpg", content_type: "image/jpg")
+# inspo19.save!
 
-inspo20 = Inspo.new(name: "Street Art Hunt", genre: "date", content: "Where's Waldo: Street Art Edition")
-file20 = URI.open("https://www.mtl.org/sites/default/files/styles/hero/public/2022-04/44730_1.jpg?itok=qu90_lwC")
-inspo20.photo.attach(io: file20, filename: "inspo20.jpg", content_type: "image/jpg")
-inspo20.save!
+# inspo20 = Inspo.new(name: "Street Art Hunt", genre: "date", content: "Where's Waldo: Street Art Edition")
+# file20 = URI.open("https://www.mtl.org/sites/default/files/styles/hero/public/2022-04/44730_1.jpg?itok=qu90_lwC")
+# inspo20.photo.attach(io: file20, filename: "inspo20.jpg", content_type: "image/jpg")
+# inspo20.save!
 
-inspo21 = Inspo.new(name: "Kobo Clara HD", genre: "gift", content: "The perfect reading companion for any book lover", cost: 150.00)
-file21 = URI.open("https://p0.pikist.com/photos/451/474/kobo-reading-light-ebook-hammock-reading-reader-tablet-digital-holiday.jpg")
-inspo21.photo.attach(io: file21, filename: "inspo21.jpg", content_type: "image/jpg")
-inspo21.save!
+# inspo21 = Inspo.new(name: "Kobo Clara HD", genre: "gift", content: "The perfect reading companion for any book lover", cost: 150.00)
+# file21 = URI.open("https://p0.pikist.com/photos/451/474/kobo-reading-light-ebook-hammock-reading-reader-tablet-digital-holiday.jpg")
+# inspo21.photo.attach(io: file21, filename: "inspo21.jpg", content_type: "image/jpg")
+# inspo21.save!
 
-inspo22 = Inspo.new(name: "Dispatch Coffee", genre: "gift", content: "Coffee that tastes good and does good", cost: 30.00)
-file22 = URI.open("https://dispatch.imgix.net/5hEq85BJ4gJttltNgM6miA/4798683d11b88e490a26542c58e05e8a/Dispatch-aug004.jpg?auto=format,compress&w=1800")
-inspo22.photo.attach(io: file22, filename: "inspo22.jpg", content_type: "image/jpg")
-inspo22.save!
+# inspo22 = Inspo.new(name: "Dispatch Coffee", genre: "gift", content: "Coffee that tastes good and does good", cost: 30.00)
+# file22 = URI.open("https://dispatch.imgix.net/5hEq85BJ4gJttltNgM6miA/4798683d11b88e490a26542c58e05e8a/Dispatch-aug004.jpg?auto=format,compress&w=1800")
+# inspo22.photo.attach(io: file22, filename: "inspo22.jpg", content_type: "image/jpg")
+# inspo22.save!
 
-inspo23 = Inspo.new(name: "AirPods 3rd Generation", genre: "gift", content: "You can even personalize an engraving!", cost: 239.00)
-file23 = URI.open("https://www.headphonesty.com/wp-content/uploads/2019/10/Apple-AirPods-Pro-Featured-Image.jpg")
-inspo23.photo.attach(io: file23, filename: "inspo23.jpg", content_type: "image/jpg")
-inspo23.save!
+# inspo23 = Inspo.new(name: "AirPods 3rd Generation", genre: "gift", content: "You can even personalize an engraving!", cost: 239.00)
+# file23 = URI.open("https://www.headphonesty.com/wp-content/uploads/2019/10/Apple-AirPods-Pro-Featured-Image.jpg")
+# inspo23.photo.attach(io: file23, filename: "inspo23.jpg", content_type: "image/jpg")
+# inspo23.save!
 
-inspo24 = Inspo.new(name: "Karaoke Night", genre: "date", content: "Have a date at Le Date", cost: 50.00, location: "1218 St Catherine St E, Montreal, Quebec")
-file24 = URI.open("https://api.time.com/wp-content/uploads/2019/09/karaoke-mic.jpg?quality=85&w=1600")
-inspo24.photo.attach(io: file24, filename: "inspo24.jpg", content_type: "image/jpg")
-inspo24.save!
+# inspo24 = Inspo.new(name: "Karaoke Night", genre: "date", content: "Have a date at Le Date", cost: 50.00, location: "1218 St Catherine St E, Montreal, Quebec")
+# file24 = URI.open("https://api.time.com/wp-content/uploads/2019/09/karaoke-mic.jpg?quality=85&w=1600")
+# inspo24.photo.attach(io: file24, filename: "inspo24.jpg", content_type: "image/jpg")
+# inspo24.save!
 
-inspo25 = Inspo.new(name: "Yoga with Surya", genre: "date", content: "The best class is on Wednesdays from 12:30-1:00pm at Le Wagon with Bernard", cost: 20.00, location: "5333 Av. Casgrain, Montréal, Quebec")
-file25 = URI.open("https://blog.goodlifefitness.com/media/1456/12008.jpg")
-inspo25.photo.attach(io: file25, filename: "inspo25.jpg", content_type: "image/jpg")
-inspo25.save!
+# inspo25 = Inspo.new(name: "Yoga with Surya", genre: "date", content: "The best class is on Wednesdays from 12:30-1:00pm at Le Wagon with Bernard", cost: 20.00, location: "5333 Av. Casgrain, Montréal, Quebec")
+# file25 = URI.open("https://blog.goodlifefitness.com/media/1456/12008.jpg")
+# inspo25.photo.attach(io: file25, filename: "inspo25.jpg", content_type: "image/jpg")
+# inspo25.save!
 
 puts "Creating keywords"
 
@@ -248,44 +248,44 @@ keyword12.save!
 
 puts "Hardcoding InspoKeywords 'cause we're hacks"
 
-ik1 = InspoKeyword.new(); ik1.inspo = inspo1; ik1.keyword = keyword6; ik1.save!
-ik2 = InspoKeyword.new(); ik2.inspo = inspo2; ik2.keyword = keyword2; ik2.save!
-ik3 = InspoKeyword.new(); ik3.inspo = inspo3; ik3.keyword = keyword9; ik3.save!
-ik4 = InspoKeyword.new(); ik4.inspo = inspo3; ik4.keyword = keyword11; ik4.save!
-Keyword.all.each do |keyword|
-  ik5 = InspoKeyword.new(); ik5.inspo = inspo4; ik5.keyword = keyword; ik5.save!
-end
-ik17 = InspoKeyword.new(); ik17.inspo = inspo5; ik17.keyword = keyword5; ik17.save!
-ik18 = InspoKeyword.new(); ik18.inspo = inspo6; ik18.keyword = keyword11; ik18.save!
-ik19 = InspoKeyword.new(); ik19.inspo = inspo7; ik19.keyword = keyword6; ik19.save!
-ik20 = InspoKeyword.new(); ik20.inspo = inspo7; ik20.keyword = keyword8; ik20.save!
-Keyword.all.each do |keyword|
-  ik21 = InspoKeyword.new(); ik21.inspo = inspo8; ik21.keyword = keyword; ik21.save!
-end
-ik33 = InspoKeyword.new(); ik33.inspo = inspo9; ik33.keyword = keyword1; ik33.save!
-ik34 = InspoKeyword.new(); ik34.inspo = inspo9; ik34.keyword = keyword3; ik34.save!
+# ik1 = InspoKeyword.new(); ik1.inspo = inspo1; ik1.keyword = keyword6; ik1.save!
+# ik2 = InspoKeyword.new(); ik2.inspo = inspo2; ik2.keyword = keyword2; ik2.save!
+# ik3 = InspoKeyword.new(); ik3.inspo = inspo3; ik3.keyword = keyword9; ik3.save!
+# ik4 = InspoKeyword.new(); ik4.inspo = inspo3; ik4.keyword = keyword11; ik4.save!
+# Keyword.all.each do |keyword|
+#   ik5 = InspoKeyword.new(); ik5.inspo = inspo4; ik5.keyword = keyword; ik5.save!
+# end
+# ik17 = InspoKeyword.new(); ik17.inspo = inspo5; ik17.keyword = keyword5; ik17.save!
+# ik18 = InspoKeyword.new(); ik18.inspo = inspo6; ik18.keyword = keyword11; ik18.save!
+# ik19 = InspoKeyword.new(); ik19.inspo = inspo7; ik19.keyword = keyword6; ik19.save!
+# ik20 = InspoKeyword.new(); ik20.inspo = inspo7; ik20.keyword = keyword8; ik20.save!
+# Keyword.all.each do |keyword|
+#   ik21 = InspoKeyword.new(); ik21.inspo = inspo8; ik21.keyword = keyword; ik21.save!
+# end
+# ik33 = InspoKeyword.new(); ik33.inspo = inspo9; ik33.keyword = keyword1; ik33.save!
+# ik34 = InspoKeyword.new(); ik34.inspo = inspo9; ik34.keyword = keyword3; ik34.save!
 
-ik35 = InspoKeyword.new(); ik35.inspo = inspo16; ik35.keyword = keyword1; ik35.save!
-ik36 = InspoKeyword.new(); ik36.inspo = inspo17; ik36.keyword = keyword2; ik36.save!
-ik37 = InspoKeyword.new(); ik37.inspo = inspo18; ik37.keyword = keyword4; ik37.save!
-ik38 = InspoKeyword.new(); ik38.inspo = inspo19; ik38.keyword = keyword5; ik38.save!
-ik39 = InspoKeyword.new(); ik39.inspo = inspo20; ik39.keyword = keyword6; ik39.save!
-ik40 = InspoKeyword.new(); ik40.inspo = inspo21; ik40.keyword = keyword7; ik40.save!
-ik41 = InspoKeyword.new(); ik41.inspo = inspo22; ik41.keyword = keyword9; ik41.save!
-ik42 = InspoKeyword.new(); ik42.inspo = inspo23; ik42.keyword = keyword10; ik42.save!
-ik43 = InspoKeyword.new(); ik43.inspo = inspo24; ik43.keyword = keyword10; ik43.save!
+# ik35 = InspoKeyword.new(); ik35.inspo = inspo16; ik35.keyword = keyword1; ik35.save!
+# ik36 = InspoKeyword.new(); ik36.inspo = inspo17; ik36.keyword = keyword2; ik36.save!
+# ik37 = InspoKeyword.new(); ik37.inspo = inspo18; ik37.keyword = keyword4; ik37.save!
+# ik38 = InspoKeyword.new(); ik38.inspo = inspo19; ik38.keyword = keyword5; ik38.save!
+# ik39 = InspoKeyword.new(); ik39.inspo = inspo20; ik39.keyword = keyword6; ik39.save!
+# ik40 = InspoKeyword.new(); ik40.inspo = inspo21; ik40.keyword = keyword7; ik40.save!
+# ik41 = InspoKeyword.new(); ik41.inspo = inspo22; ik41.keyword = keyword9; ik41.save!
+# ik42 = InspoKeyword.new(); ik42.inspo = inspo23; ik42.keyword = keyword10; ik42.save!
+# ik43 = InspoKeyword.new(); ik43.inspo = inspo24; ik43.keyword = keyword10; ik43.save!
 
-ik44 = InspoKeyword.new(); ik44.inspo = inspo10; ik44.keyword = keyword3; ik44.save!
-ik45 = InspoKeyword.new(); ik45.inspo = inspo11; ik45.keyword = keyword4; ik45.save!
-ik46 = InspoKeyword.new(); ik46.inspo = inspo12; ik46.keyword = keyword7; ik46.save!
-ik47 = InspoKeyword.new(); ik47.inspo = inspo12; ik47.keyword = keyword8; ik47.save!
-ik48 = InspoKeyword.new(); ik48.inspo = inspo13; ik48.keyword = keyword3; ik48.save!
-ik49 = InspoKeyword.new(); ik49.inspo = inspo14; ik49.keyword = keyword12; ik49.save!
+# ik44 = InspoKeyword.new(); ik44.inspo = inspo10; ik44.keyword = keyword3; ik44.save!
+# ik45 = InspoKeyword.new(); ik45.inspo = inspo11; ik45.keyword = keyword4; ik45.save!
+# ik46 = InspoKeyword.new(); ik46.inspo = inspo12; ik46.keyword = keyword7; ik46.save!
+# ik47 = InspoKeyword.new(); ik47.inspo = inspo12; ik47.keyword = keyword8; ik47.save!
+# ik48 = InspoKeyword.new(); ik48.inspo = inspo13; ik48.keyword = keyword3; ik48.save!
+# ik49 = InspoKeyword.new(); ik49.inspo = inspo14; ik49.keyword = keyword12; ik49.save!
 
-ik50 = InspoKeyword.new(); ik50.inspo = inspo25; ik50.keyword = keyword12; ik50.save!
-Keyword.all.each do |keyword|
-  ik51 = InspoKeyword.new(); ik51.inspo = inspo15; ik51.keyword = keyword; ik51.save!
-end
+# ik50 = InspoKeyword.new(); ik50.inspo = inspo25; ik50.keyword = keyword12; ik50.save!
+# Keyword.all.each do |keyword|
+#   ik51 = InspoKeyword.new(); ik51.inspo = inspo15; ik51.keyword = keyword; ik51.save!
+# end
 
 
 # event1 = Event.new(date: Time.new(2022, 8, 28, 19), content: "text")
