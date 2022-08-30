e20d3da6b944245df09f9e4483c15c97daa06fcb
Michael Wales <wales@aya.yale.edu>
2022-08-30 06:39:39 -0700

    change branch

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,4 +23,16 @@ class Event < ApplicationRecord
       end
     end
   end
+
+  def send_message
+    require 'twilio-ruby'
+    account_sid = 'ACbf7f4e607a174f62aebf4c5a3a6fab20'
+    auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyy'
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = '+15551234567' # Your Twilio number
+    to = '+15555555555' # Your mobile phone number
+
+    client.messages.create(from: from, to: to, body: "Hey friend!")
+  end
 end
