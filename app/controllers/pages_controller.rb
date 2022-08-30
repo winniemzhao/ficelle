e6afd9b3161a56e22d6346f5b13dda4273fa89c1
Michael Wales <wales@aya.yale.edu>
2022-08-29 22:01:25 -0700

    Please for the love of all that is holy, approve the prior pull requests first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user && current_user.partner
+      @path = dashboard_path
+      @message = 'Go to dashboard'
+    elsif current_user
+      @path = new_partner_path
+      @message = 'Create a partner'
+    else
+      @path = user_session_path
+      @message = 'Get started'
+    end
   end
 end
