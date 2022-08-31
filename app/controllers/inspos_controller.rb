31e66f76e536968bb1b80e06be12caa60566570d
Winnie Zhao <winniemaizhao@gmail.com>
2022-08-31 17:37:26 -0400

    replaced everything we did to test

--- app/controllers/inspos_controller.rb
+++ app/controllers/inspos_controller.rb
@@ -1,14 +1,13 @@
 class InsposController < ApplicationController
   def index
-    @inspos = Inspo.all
-    # if current_user && current_user.partner
-    #   @inspos = current_user.partner.inspos.uniq.reject do |inspo|
-    #     inspo.favorited_by?(current_user)
-    #   end.reject do |inspo|
-    #     current_user.blocked_by?(inspo)
-    #   end.sample(5)
-    #   @inspos = Inspo.all.sample(5) if @inspos.empty?
-    # end
+    if current_user && current_user.partner
+      @inspos = current_user.partner.inspos.uniq.reject do |inspo|
+        inspo.favorited_by?(current_user)
+      end.reject do |inspo|
+        current_user.blocked_by?(inspo)
+      end.sample(5)
+      @inspos = Inspo.all.sample(5) if @inspos.empty?
+    end
   end
 
   def toggle_favorite
