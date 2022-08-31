ffcf526499cbfe6b148e8a8abed460e8854bc166
Michael Wales <wales@aya.yale.edu>
2022-08-31 11:58:58 -0700

    last push

--- app/controllers/inspos_controller.rb
+++ app/controllers/inspos_controller.rb
@@ -1,13 +1,12 @@
 class InsposController < ApplicationController
   def index
-    if current_user.partner.nil?
-      redirect_to new_partner_path
-    else
+    if current_user && current_user.partner
       @inspos = current_user.partner.inspos.uniq.reject do |inspo|
         inspo.favorited_by?(current_user)
       end.reject do |inspo|
         current_user.blocked_by?(inspo)
       end.sample(5)
+      @inspos = Inspo.all.sample(5) if @inspos.empty?
     end
   end
 
