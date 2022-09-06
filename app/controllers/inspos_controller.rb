bbe29a5b9489b18c7095021e0f3a06d6a0f40470
Winnie Zhao <winniemaizhao@gmail.com>
2022-09-06 15:13:17 -0400

    undid hardcoding

--- app/controllers/inspos_controller.rb
+++ app/controllers/inspos_controller.rb
@@ -1,15 +1,15 @@
 class InsposController < ApplicationController
   def index
     if current_user && current_user.partner
-    #   @inspos = current_user.partner.inspos.uniq.reject do |inspo|
-    #     inspo.favorited_by?(current_user)
-    #   end.reject do |inspo|
-    #     current_user.blocked_by?(inspo)
-    #   end.sample(5)
-    #   @inspos = Inspo.all.sample(5) if @inspos.empty?
-      inspos = [Inspo.where(name: "Netflix and Chill")[0], Inspo.where(name: "Fishing Rod")[0], Inspo.where(name: "Send a Love Poem")[0]]
-      randos = Inspo.where.not(name: "Netflix and Chill").where.not(name: "Fishing Rod").where.not(name: "Send a Love Poem").sample(2)
-      @inspos = inspos | randos
+      @inspos = current_user.partner.inspos.uniq.reject do |inspo|
+        inspo.favorited_by?(current_user)
+      end.reject do |inspo|
+        current_user.blocked_by?(inspo)
+      end.sample(5)
+      @inspos = Inspo.all.sample(5) if @inspos.empty?
+      # inspos = [Inspo.where(name: "Netflix and Chill")[0], Inspo.where(name: "Fishing Rod")[0], Inspo.where(name: "Send a Love Poem")[0]]
+      # randos = Inspo.where.not(name: "Netflix and Chill").where.not(name: "Fishing Rod").where.not(name: "Send a Love Poem").sample(2)
+      # @inspos = inspos | randos
     end
   end
 
